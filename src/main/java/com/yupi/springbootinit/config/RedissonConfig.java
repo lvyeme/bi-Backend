7c66188dc15868a570f575818775e67ba71459d6
Sagasu <62923489+lvyeme@users.noreply.github.com>
2023-10-28 23:46:42 -0500

    增加用户上传文件大小及其类型限制

---
+++ src/main/java/com/yupi/springbootinit/config/RedissonConfig.java
@@ -0,0 +1,32 @@
+package com.yupi.springbootinit.config;
+
+import lombok.Data;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author LVye
+ * @version 1.0
+ */
+@ConfigurationProperties(prefix = "spring.redis")
+@Data
+@Configuration
+public class RedissonConfig {
+    private Integer database;
+    private String host;
+    private Integer port;
+    private String password;
+    // Sync and Async API
+    public RedissonClient getRedissonClient(){
+        Config config = new Config();
+        config.useSingleServer()
+                .setDatabase(database)
+                .setAddress("redis//" + host + ":" + port);
+//                .setPassword(password);
+        RedissonClient redisson = Redisson.create(config);
+        return redisson;
+    }
+}
