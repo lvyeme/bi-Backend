2ea0b568cd823b50654037a56fca1333063d45ee
Sagasu <62923489+lvyeme@users.noreply.github.com>
2023-11-11 23:50:08 +0800

    rabbitMq

---
+++ src/test/java/com/yupi/springbootinit/mapper/ChartMapperTest.java
@@ -0,0 +1,22 @@
+package com.yupi.springbootinit.mapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+
+/**
+ * @author LVye
+ * @version 1.0
+ */
+@SpringBootTest
+class ChartMapperTest {
+    @Resource
+    private ChartMapper chartMapper;
+
+    @Test
+    void queryChartData() {
+
+
+    }
+}
